179a3f3ffe547cd25f89ce6117b431411b044321
nikolaspapirniywork <nikolaspapirniywork@gmail.com>
2013-12-09 13:57:27 +0200

    New solutions for P4

--- haskell/src/P4.hs
+++ haskell/src/P4.hs
@@ -23,3 +23,23 @@ myLength' list = foldr (\_ acc -> 1 + acc) 0 list
 myLength'' :: (Num a) => [a] -> Int
 myLength'' [] = 0
 myLength'' list = foldl (\acc _ -> 1 + acc) 0 list
+
+myLength''' :: [a] -> Int
+myLength''' list = mylist_acc 0 list
+                where 
+                        mylist_acc :: Int -> [a] -> Int
+                        mylist_acc acc [] = acc
+                        mylist_acc acc (_:xs) = mylist_acc (acc + 1) xs
+                        
+myLength'''' :: [a] -> Int
+myLength'''' list =
+                let
+                        mylist_acc :: [a] -> Int -> Int
+                        mylist_acc [] acc  = acc
+                        mylist_acc (_:xs) acc = mylist_acc xs (acc + 1)
+                in        
+                        mylist_acc list 0             
+                        
+myLength''''' :: [a] -> Int 
+myLength''''' list = sum $ map (\_ -> 1) list                      
+                        
