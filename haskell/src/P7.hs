1559f61febfc9dfa81baca0db130996e37fb3ee9
PAPIRNIY NIKOLAY <nikolaspapirniywork@gmail.com>
2013-12-15 15:56:09 +0200

    Problem 7

---
+++ haskell/src/P7.hs
@@ -0,0 +1,39 @@
+module P7 where
+
+{-
+    (**) Flatten a nested list structure.
+        
+    Transform a list, possibly holding lists as elements into a `flat' list by replacing each list with its elements (recursively).
+        
+    Example:
+        
+    * (my-flatten '(a (b (c d) e)))
+    (A B C D E)
+        
+    We have to define a new data type, because lists in Haskell are homogeneous.
+
+     data NestedList a = Elem a | List [NestedList a]
+    *Main> flatten (Elem 5)
+    [5]
+    *Main> flatten (List [Elem 1, List [Elem 2, List [Elem 3, Elem 4], Elem 5]])
+    [1,2,3,4,5]
+    *Main> flatten (List [])
+    []
+-}
+
+
+data NestedList a = Elem a | List [NestedList a] deriving (Show)
+
+flatten' :: NestedList a -> [a]
+flatten' (Elem e) = [e]
+flatten' (List list) = concatMap flatten' list  
+
+flatten'' :: NestedList a -> [a]
+flatten'' (List []) = []
+flatten'' (Elem element) = [element]
+flatten'' (List (x:xs)) = flatten' x ++ (flatten' $ List xs) 
+
+flatten''' :: NestedList a -> [a]
+flatten''' (Elem e) = [e]
+flatten''' (List xs) = foldr (++) [] $ map flatten''' xs
+
